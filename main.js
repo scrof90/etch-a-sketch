31dc9db369f7e44c10f55d08495213693c6703aa
scrof90 <asknyshev@gmail.com>
2021-09-07 00:31:51 +0300

    refactor functions

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const grid = document.getElementById('gridContainer');
+
 let gridWidth = 16;
 let gridHeight = 16;
 
@@ -6,28 +8,34 @@ function main() {
 }
 
 function initGrid() {
-  const grid = document.getElementById('gridContainer');
   grid.style.gridTemplateRows = `repeat(${gridWidth}, 30px [row])`;
   grid.style.gridTemplateColumns = `repeat(${gridHeight}, 30px [col])`;
-
-  for (let i = 0; i < gridWidth * gridHeight; i++) {
-    const cell = document.createElement('div');
-    cell.id = `cell_${i}`;
-    cell.addEventListener('mouseover', () => cell.style.backgroundColor = 'white');
-    grid.appendChild(cell);
-  }
+  fillGrid(gridWidth, gridHeight);
 }
 
 function resetGrid() {
-  const grid = document.getElementById('gridContainer');
+  clearGrid();
+  initGrid();
+}
 
+function clearGrid() {
   let cell = grid.lastChild;
   while (cell) {
     grid.removeChild(cell);
     cell = grid.lastChild;
   }
-
-  initGrid();
 }
 
+function fillGrid(width, height) {
+  const numOfCells = width * height;
+  for (let i = 0; i < numOfCells; i++) {
+    const cell = document.createElement('div');
+    cell.id = `cell_${i}`;
+    cell.addEventListener(
+      'mouseover',
+      () => (cell.style.backgroundColor = 'white')
+    );
+    grid.appendChild(cell);
+  }
+}
 main();
