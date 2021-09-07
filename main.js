6466c94d216f3daacd3fd6093e9e422a01f9ed2b
scrof90 <asknyshev@gmail.com>
2021-09-08 00:26:22 +0300

    refactor

--- main.js
+++ main.js
@@ -1,69 +1,63 @@
-(function () {
-  const grid = document.getElementById('gridContainer');
+const grid = document.getElementById('gridContainer');
 
-  let gridDensity = 20;
-  let paintColor = '#000000';
+let gridDensity = 20;
+let paintColor = '#000000';
 
-  main();
+initSlider();
+initColorPicker();
+initResetButton();
+resetGrid();
 
-  function main() {
-    initSlider();
-    initColorPicker();
-    initResetButton();
-    resetGrid();
+function initSlider() {
+  const densitySlider = document.getElementById('density');
+  densitySlider.oninput = function () {
+    gridDensity = this.value;
 
-    function initSlider() {
-      const densitySlider = document.getElementById('density');
-      densitySlider.oninput = function () {
-        gridDensity = this.value;
+    const densityLabel = document.getElementById('densityLabel');
+    densityLabel.innerText = `${gridDensity}x${gridDensity}`;
+  };
+}
 
-        const densityLabel = document.getElementById('densityLabel');
-        densityLabel.innerText = `${gridDensity}x${gridDensity}`;
-      };
-    }
+function initColorPicker() {
+  const colorPicker = document.getElementById('color');
+  colorPicker.oninput = function () {
+    paintColor = this.value;
+  };
+}
 
-    function initColorPicker() {
-      const colorPicker = document.getElementById('color');
-      colorPicker.oninput = function () {
-        paintColor = this.value;
-      };
-    }
+function initResetButton() {
+  const resetButton = document.getElementById('reset');
+  resetButton.onclick = () => resetGrid();
+}
 
-    function initResetButton() {
-      const resetButton = document.getElementById('reset');
-      resetButton.onclick = () => resetGrid();
-    }
+function resetGrid() {
+  clearGrid();
+  grid.style.gridTemplateRows = `repeat(${gridDensity}, auto [row])`;
+  grid.style.gridTemplateColumns = `repeat(${gridDensity}, auto [col])`;
+  fillGrid();
+}
+
+function clearGrid() {
+  let cell = grid.lastChild;
+  while (cell) {
+    grid.removeChild(cell);
+    cell = grid.lastChild;
   }
+}
 
-  function resetGrid() {
-    clearGrid();
-    grid.style.gridTemplateRows = `repeat(${gridDensity}, auto [row])`;
-    grid.style.gridTemplateColumns = `repeat(${gridDensity}, auto [col])`;
-    fillGrid();
+function fillGrid() {
+  const numOfCells = gridDensity ** 2;
+  for (let i = 0; i < numOfCells; i++) {
+    const cell = getNewCell();
+    grid.appendChild(cell);
   }
+}
 
-  function clearGrid() {
-    let cell = grid.lastChild;
-    while (cell) {
-      grid.removeChild(cell);
-      cell = grid.lastChild;
-    }
-  }
-
-  function fillGrid() {
-    const numOfCells = gridDensity ** 2;
-    for (let i = 0; i < numOfCells; i++) {
-      const cell = getNewCell();
-      grid.appendChild(cell);
-    }
-
-    function getNewCell() {
-      const cell = document.createElement('div');
-      cell.addEventListener('mouseover', function () {
-        if (this.style.backgroundColor === paintColor) return;
-        this.style.backgroundColor = paintColor;
-      });
-      return cell;
-    }
-  }
-})();
+function getNewCell() {
+  const cell = document.createElement('div');
+  cell.addEventListener('mouseover', function () {
+    if (this.style.backgroundColor === paintColor) return;
+    this.style.backgroundColor = paintColor;
+  });
+  return cell;
+}
